622e92030e6d72cdedf4bd0ec9e580526dbe2467
Anastasiya Grigorenko <you@example.com>
2021-12-09 22:29:38 +0400

    task.js was changed

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    let dropdown_list = document.querySelector(".dropdown__list");
+    let button = document.querySelector(".dropdown__value");
+    let dropdown_item = document.querySelectorAll(".dropdown__item");
+  
+    button.addEventListener('click', function (event) {
+        dropdown_list.classList.toggle('dropdown__list_active');
+    });
+    
+    
+    let itemsArr = Array.from(dropdown_item);
+    
+    for (i = 0; i < itemsArr.length; i++) {
+        let firstEl = itemsArr[0];
+        let secondEl = itemsArr[1];
+        let thirdEl = itemsArr[2];
+        let forthEl = itemsArr[3];
+        
+        firstEl.addEventListener('click', function () {
+         button.textContent = 'JavaScript';
+        });
+        
+        secondEl.addEventListener('click', function () {
+        button.textContent = 'PHP';
+        });
+        
+        thirdEl.addEventListener('click', function () {
+        button.textContent = 'Python';
+        });
+        
+        forthEl.addEventListener('click', function () {
+        button.textContent = 'Fortran';
+        });
+        
+    }
+ 
+    
+    
+    
+});
