86a1cc7f00a73c25b361f2090b936d00ef89cce5
Anastasiya Grigorenko <you@example.com>
2021-12-12 16:38:52 +0400

    task.js was changed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -6,20 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
     main_links.forEach(function(item) {
         item.addEventListener('click', function (e) {
                         
-            //let hasSubMenu = item.closest(".menu__item").querySelector(".menu_sub");
+            let hasParent = item.closest(".menu__item");
             
-            //if (hasSubMenu === true) {
-            //    hasSubMenu.classList.toggle("menu_active");
-            //}
-            
-            let menu_item = document.getElementsByName("menu__item");
-            let menu_sub = document.getElementsByClassName("menu_sub");
-            
-            if (menu_item.contains(menu_sub) === true) {
-                menu_sub.classList.toggle("menu_active");
+            if (hasParent) {
+                let sub_menu = hasParent.querySelector(".menu_sub");
+                sub_menu.classList.toggle("menu_active");
             }
                    
-           return false;
+        e.preventDefault();
                         
         });
     });
