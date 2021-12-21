4741e453d5a12d67f4fd9b939ac4f4da1bb3f655
Anastasiya Grigorenko <you@example.com>
2021-12-21 22:26:28 +0400

    task.js was changed

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -15,49 +15,19 @@ document.addEventListener('DOMContentLoaded', function () {
         
         itemsArr[i].addEventListener('click', function (e) {
             
-        if (this.textContent === "JavaScript") {
+        let text = this.textContent.trim();
+                                    
+        if (text === "JavaScript") {
             button.textContent = 'JavaScript';
-        } else if (this.textContent === "PHP") {
+        } else if (text === "PHP") {
             button.textContent = 'PHP';
-        } else if (this.textContent === "Python") {
+        } else if (text === "Python") {
             button.textContent = 'Python';
-        } else if (this.textContent === "Fortran") {
+        } else if (text === "Fortran") {
             button.textContent = 'Fortran';
         }   
             
         e.preventDefault();
        });      
-    }    
-    
-    /*
-    let itemsArr = Array.from(dropdown_item);
-    
-    for (i = 0; i < itemsArr.length; i++) {
-        let firstEl = itemsArr[0];
-        let secondEl = itemsArr[1];
-        let thirdEl = itemsArr[2];
-        let forthEl = itemsArr[3];
-        
-        firstEl.addEventListener('click', function () {
-         button.textContent = 'JavaScript';
-        });
-        
-        secondEl.addEventListener('click', function () {
-        button.textContent = 'PHP';
-        });
-        
-        thirdEl.addEventListener('click', function () {
-        button.textContent = 'Python';
-        });
-        
-        forthEl.addEventListener('click', function () {
-        button.textContent = 'Fortran';
-        });
-        
-    }
-    */
- 
-    
-    
-    
+    }       
 });
