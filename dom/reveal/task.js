d76a69c72dbdad24bf1181bb956681102b20f89e
Anastasiya Grigorenko <you@example.com>
2022-01-17 23:44:51 +0400

    task.js was added

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     
     document.addEventListener("scroll", function (){
-        let block = document.querySelector('.reveal');
-    
+        let block = document.getElementsByClassName('reveal');
+            
         let isInViewport = function(element){
             const viewPortHeight = window.innerHeight;
             const elementTop = element.getBoundingClientRect().top;
             
             return elementTop < viewPortHeight ? true : false;
         };
+        
+        for (let i = 0; i < block.length; i++) {
                 
-        if (isInViewport(block) === true) {
-            block.classList.add("reveal_active");
+        if (isInViewport(block[i]) === true) {
+            block[i].classList.add("reveal_active");
         } else {
-             block.classList.remove("reveal_active");
+             block[i].classList.remove("reveal_active");
+        }
         }
 
     });
