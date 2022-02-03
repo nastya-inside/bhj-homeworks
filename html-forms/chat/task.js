65a936f2ee7ee5752aa3308b46f4f7a66ff0d14f
Anastasiya Grigorenko <you@example.com>
2022-02-03 19:37:25 +0400

    task.js was changed

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     
-   let chat = document.querySelector('.chat-widget');
+   let widget = document.querySelector('.chat-widget');
    let time = document.querySelectorAll('.message__time');
     
     //откроем чат по клику
-    chat.addEventListener('click', function() {
+    widget.addEventListener('click', function() {
         this.classList.add('chat-widget_active');
     });
     
@@ -18,41 +18,41 @@ document.addEventListener('DOMContentLoaded', function () {
     let input = document.querySelector('input');
             
     input.addEventListener('keyup', function(e) {
-        let client = document.querySelector('.message_client');
         let clientMessage = document.querySelector('.message_client > .message__text');
+        let chat = document.getElementById('chat-widget__messages');
+        let input = document.getElementById('chat-widget__input');
+        
+        //добавим сообщения и организуем рандомный выбор
+        const messages = document.querySelector('.chat-widget__messages');
+        let message = document.getElementsByClassName('message');
+        const messagesArr = [
+            'Я не выспался',
+            'Ничего не знаю, моя хата с краю',
+            'Пока',
+            'Мужчина, вы что не видите, у нас обед'
+        ];
+    
+        let messageArr = messagesArr[Math.floor(Math.random()*messagesArr.length)];
         
         if (e.keyCode == 13) {
-            clientMessage.textContent = input.value;
-        }
-        
-        for (let i = 0; i < clientMessage.length; i++) {
-        clientMessage[i].innerHTML += `
+        chat.innerHTML += `
         <div class="message message_client">
         <div class="message__time">${currentTime}</div>
-        <div class="message__text">${input.value}</div>
+        <div class="message__text">${e.target.value}</div>
+        </div>
+        `; 
+        
+        messages.innerHTML += `
+        <div class="message">
+        <div class="message__time">${currentTime}</div>
+        <div class="message__text">${messageArr}</div>
         </div>
         `;
-        }
         
-    });
-    
-    //добавим сообщения 
-    const messages = document.querySelector('.chat-widget__messages');
-    let message = document.getElementsByClassName('message');
-    
-    messages.innerHTML += `
-     <div class="message">
-     <div class="message__time">${currentTime}</div>
-     <div class="message__text">Я не выспался</div>
-     </div>
-     <div class="message">
-     <div class="message__time">${currentTime}</div>
-     <div class="message__text">Ничего не знаю, моя хата с краю</div>
-     </div>
-     <div class="message">
-     <div class="message__time">${currentTime}</div>
-     <div class="message__text">Пока</div>
-     </div>
-    `;
-    
+        //проверка на пустое значение 
+        if (input.value == ' '){
+            e.preventDefault();
+          }
+        }    
+    });   
 });
