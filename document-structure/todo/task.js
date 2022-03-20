b2995bdd936f6e45ba2ada4167924dc0057bb615
Anastasiya Grigorenko <you@example.com>
2022-03-20 22:39:23 +0400

    task.js was added

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,42 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let removeButton = document.getElementsByClassName('task__remove');
-    let elementOfList = document.querySelector('.task');
-    let btn = document.querySelector(".tasks__add");
+    const btn = document.querySelector(".tasks__add");
     
     //создадим функцию, которая будет добавлять новый эл-т списка 
     function newElementOfList () {
-        let div = document.createElement("div");
+        const div = document.createElement("div");
         let inputValue = document.getElementById("task__input").value;
-        let t = document.createTextNode(inputValue);
+        const t = document.createTextNode(inputValue);
         div.classList.add("task__title");
         div.appendChild(t);
         
     //новый контейнер task
-        let container = document.createElement("div");
+        const container = document.createElement("div");
         container.classList.add("task");
-            
-        if (inputValue === '') {
-            alert ('You have to type something dude');
+        
+        if (inputValue.charAt(0) === '' || inputValue.charAt(0) === ' ') {
+            inputValue = '';
         } else {
             document.getElementById("tasks__list").appendChild(container);
             container.appendChild(div);
-        }
+       }
         
         //создадим ссылку, которая будет являться типо кнопкой закрытия
-        let a = document.createElement("a");
+        const a = document.createElement("a");
         a.classList.add("task__remove");
         document.getElementById("tasks__list").appendChild(a);
         a.innerHTML = "&times;";
         container.appendChild(a);
       
         
-        //закрытие элемента списка по клику на кнопку 
-        for (let i = 0; i < a.length; i++) {
-        a[i].addEventListener("click", function () {
-        let element = this.parentElement;
-        element.style.display = "none";
-        });    
-    } 
+        a.addEventListener('click', function () {
+            container.remove();
+        });
+        
         //очистим инпут
         document.getElementById("task__input").value = "";
     } 
