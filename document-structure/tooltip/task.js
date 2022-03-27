447602a79623c550a0d571d4175ae658c3c1c6d3
Anastasiya Grigorenko <you@example.com>
2022-03-28 00:40:51 +0400

    task.js was changed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,48 +2,48 @@ document.addEventListener('DOMContentLoaded', function () {
     let links = document.querySelectorAll('.has-tooltip');
     
     //создадим элемент подсказки 
-    const $tooltip = document.createElement("div")
-    $tooltip.classList.add("tooltip")
+    const tooltip = document.createElement("div")
+    tooltip.classList.add("tooltip")
     
     // Ссылка, которую нажали последний раз
     let clickedLink = null
     
     for (let i = 0; i < links.length; i++){
-        const $link = links[i]
+        const link = links[i]
         
-        $link.onclick = function (event) {
+        link.onclick = function (event) {
         event.preventDefault()
         
         //сравнение запомненной ссылки с нажатой
-        if (clickedLink === $link) {
-        hideTooltip($tooltip)
+        if (clickedLink === link) {
+        hideTooltip(tooltip)
         clickedLink = null
       } else {
-        clickedLink = $link
-        showTooltip($link, $tooltip)
+        clickedLink = link
+        showTooltip(link, tooltip)
       }
     }
     }   
 });
 
-function showTooltip($link, $tooltip) {
-  const linkPosition = $link.getBoundingClientRect()
+function showTooltip(link, tooltip) {
+  const linkPosition = link.getBoundingClientRect()
 
   // Вставка подсказки в DOM
-  const $parent = $link.parentNode
-  $parent.insertBefore($tooltip, $link)
-  $tooltip.innerHTML = $link.getAttribute("title")
+  const parent = link.parentNode
+  parent.insertBefore(tooltip, link)
+  tooltip.innerHTML = link.getAttribute("title")
 
   // Расчет позиции подсказки
   let top = linkPosition.top
   let left = linkPosition.left
 
   // Показать подсказку
-  $tooltip.classList.add("tooltip_active")
+  tooltip.classList.add("tooltip_active")
 
   // Добавление смещения если указано положение подсказки
-  const tooltipPositionType = $link.dataset.position
-  const tooltipPosition = $tooltip.getBoundingClientRect()
+  const tooltipPositionType = link.dataset.position
+  const tooltipPosition = tooltip.getBoundingClientRect()
   if (tooltipPositionType) {
     if (tooltipPositionType === "top") {
       top -= tooltipPosition.height
@@ -59,11 +59,11 @@ function showTooltip($link, $tooltip) {
   }
 
   // Установка позиции подсказки
-  $tooltip.style.top = top + "px"
-  $tooltip.style.left = left + "px"
+  tooltip.style.top = top + "px"
+  tooltip.style.left = left + "px"
 }
 
-function hideTooltip($tooltip) {
-  $tooltip.classList.remove("tooltip_active")
+function hideTooltip(tooltip) {
+  tooltip.classList.remove("tooltip_active")
 }
 
