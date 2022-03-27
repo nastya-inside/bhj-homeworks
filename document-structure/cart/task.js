30c02b3df893428f59e93b64ad9d1425dc1000d6
Anastasiya Grigorenko <you@example.com>
2022-03-27 16:48:52 +0400

    task.js was changed

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,16 +1,50 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let plus = document.querySelector(".product__quantity-control_inc");
-    let minus = document.querySelector(".product__quantity-control_dec");
-    let value = document.querySelector(".product__quantity-value");
-    let addToCartButton = document.querySelector(".product__add");
+document.addEventListener('DOMContentLoaded', function () {    
+    //найдем продукты
+    const products = document.querySelectorAll(".product");
+    //контейнер продуктов в корзине
+    const cartProducts = document.querySelector(".cart__products");
     
-    //меняем кол-во товаров в каталоге 
-    document.addEventListener('click', function (e) {
-        if (e.target.classList.contains("product__quantity-control_inc")) {
-            ++e.target.parentElement.querySelector(".product__quantity-value").textContent;
-        } else if (e.target.classList.contains("product__quantity-control_dec")) {
-            --e.target.parentElement.querySelector(".product__quantity-value").textContent;
-        } 
-    });
-    
+    for (let i = 0; i < products.length; i++) {
+        const $product = products[i];
+        
+        const plusButton = $product.querySelector(".product__quantity-control_inc");
+        const minusButton = $product.querySelector(".product__quantity-control_dec");
+        const addToCartButton = $product.querySelector(".product__add");
+        const productsCount = $product.querySelector(".product__quantity-value");
+        
+        plusButton.onclick = function (event) {
+            event.preventDefault();
+        
+            productsCount.textContent++;
+        }
+        
+        minusButton.onclick = function (event) {
+            event.preventDefault();
+            
+            if (productsCount.textContent > 1) {
+                 productsCount.textContent--;
+            }
+        }
+        
+        addToCartButton.onclick = function (event) {
+            event.preventDefault();
+            
+            //создать новый продукт в корзине
+            const cartProduct = document.createElement("div");
+            cartProduct.classList.add("cart__product");
+            
+            //в новый продукт скопируем изображение и счетчик
+            const clonedImage = $product.querySelector(".product__image").cloneNode(true);
+            clonedImage.classList.add("cart__product-image");
+            const clonedProductCounter = $product.querySelector(".product__quantity-value").cloneNode(true);
+            clonedProductCounter.classList.add("cart__product-count");
+            
+            //добавим в новый контейнер продукта картинку и счетчик
+            cartProduct.appendChild(clonedImage);
+            cartProduct.appendChild(clonedProductCounter);
+            
+            cartProducts.appendChild(cartProduct);
+            
+        }    
+    }  
 });
